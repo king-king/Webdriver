d620cb0e702bd6e8e4561b007dccae0b77409896
qunwang <niuyiwangqun@sohu.com>
2017-03-15 21:34:40 +0800

    change

--- test/test2.js
+++
@@ -1,7 +0,0 @@
-let webdriver = require('selenium-webdriver');
-
-let driver = new webdriver.Builder().
-withCapabilities(webdriver.Capabilities.chrome()).
-build();
-
-driver.get('http://www.baidu.com')
--- test2.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by wangqun on 2017/3/12.
- */
-
-console.log(process.env['PATH'])
